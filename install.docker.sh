770a1ba940f2eb3c02afb932eb528e0960c17982
Martastain <martas@imm.cz>
2016-05-17 10:32:59 +0200

    added docker installer

---
+++ install.docker.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Copyright (c) 2016 imm studios, z.s.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+##############################################################################
+## COMMON UTILS
+
+BASEDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+TEMPDIR=/tmp/$(basename "${BASH_SOURCE[0]}")
+
+function error_exit {
+    printf "\n\033[0;31mInstallation failed\033[0m\n"
+    cd $BASEDIR
+    exit 1
+}
+
+function finished {
+    printf "\n\033[0;92mInstallation completed\033[0m\n"
+    cd $BASEDIR
+    exit 0
+}
+
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   error_exit
+fi
+
+if [ ! -d $TEMPDIR ]; then
+    mkdir $TEMPDIR || error_exit
+fi
+
+## COMMON UTILS
+##############################################################################
+
+DEB_RELEASE="jessie"
+
+function install_docker {
+
+    apt-get purge lxc-docker*
+    apt-get purge docker.io*
+    apt-get update
+    apt-get install apt-transport-https ca-certificates
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+    echo "deb https://apt.dockerproject.org/repo debian-${DEB_RELEASE} main" > /etc/apt/sources.list.d/docker.list
+    apt-get update
+    apt-get install docker-engine
+    service docker start
+
+
+}
+
+install_docker
+finished
