217944e70f079105b720d7e82a9436c79ec24fde
Martastain <martas@imm.cz>
2017-05-03 14:35:05 +0200

    added cuda installer and ffmpeg cuda support (untested)

--- install.ffmpeg.sh
+++ install.ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2015 - 2016  imm studios, z.s.
+# Copyright (c) 2015 - 2017  imm studios, z.s.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -43,22 +43,32 @@ fi
 ## COMMON UTILS
 ##############################################################################
 
-YASM_VERSION="1.3.0"
 FFMPEG_VERSION="3.3"
 VPX_VERSION="1.6.0"
 OPUS_VERSION="1.1.3"
+NVENC_VERSION="7.1.9"
+X265_VERSION="2.3"
+YASM_VERSION="1.3.0"
+
+
+if [ -d "/usr/local/cuda" ]; then
+    CUDA_PARAMS="--enable-cuda --enable-cuvid --enable-libnpp"
+    CUDA_PARAMS="$CUDA_PARAMS --extra-cflags=\"-I/usr/local/cuda/include/\""
+    CUDA_PARAMS="$CUDA_PARAMS --extra-ldflags=\"-L/usr/local/cuda/lib64/\""
+else
+    CUDA_PARAMS=""
+fi
+
 
 REPOS=(
     "https://github.com/mstorsjo/fdk-aac"
     "https://github.com/mirror/x264"
-    "https://github.com/videolan/x265"
     "https://github.com/martastain/bmd-sdk"
 )
 
 if [ -z "$PREFIX" ]; then
     PREFIX="/usr/local"
 fi
-
 
 function install_prerequisites {
     apt-get -y install\
@@ -86,19 +96,17 @@ function install_prerequisites {
         libchromaprint-dev \
         libcaca-dev \
         libfribidi-dev \
-        libgme-dev \
-        libpulse-dev \
         libmp3lame-dev \
         libmodplug-dev \
         libtwolame-dev \
         libbs2b-dev \
+        librtmp-dev \
+        librtmp1 \
         libsoxr-dev \
         libopus-dev \
         libspeex-dev \
         libssh-dev \
-        libtheora-dev \
         libv4l-dev \
-        libvorbis-dev \
         libwavpack-dev \
         libwebp-dev \
         libzvbi-dev || exit 1
@@ -143,6 +151,7 @@ function install_fdk_aac {
     return 0
 }
 
+
 function install_opus {
     cd $TEMPDIR
     wget http://downloads.xiph.org/releases/opus/opus-${OPUS_VERSION}.tar.gz
@@ -155,12 +164,17 @@ function install_opus {
     return 0
 }
 
+
 function install_nvenc {
     cd $TEMPDIR
-    wget "https://developer.nvidia.com/video-sdk-601" -O nvenc_sdk.zip
-    unzip nvenc_sdk.zip || return 1
-    cp nvidia_video_sdk_*/Samples/common/inc/*.h /usr/include/
-    cp -r nvidia_video_sdk_*/Samples/common/inc/GL /usr/include/
+    MODULE_NAME="Video_Codec_SDK_${NVENC_VERSION}"
+    wget "http://repo.imm.cz/${MODULE_NAME}.zip"
+    if [ -d ${MODULE_NAME} ]; then
+        rm -rf ${MODLE_NAME}
+    fi
+    unzip ${MODULE_NAME}.zip || return 1
+    cp -v ${MODULE_NAME}/Samples/common/inc/*.h /usr/include/
+    cp -rv ${MODULE_NAME}/Samples/common/inc/GL /usr/include/
     return 0
 }
 
@@ -197,7 +211,10 @@ function install_x264 {
 
 
 function install_x265 {
-    cd $TEMPDIR/x265
+    cd $TEMPDIR
+    wget http://ftp.videolan.org/pub/videolan/x265/x265_${X265_VERSION}.tar.gz
+    tar -xf x265_${X265_VERSION}.tar.gz
+    cd $TEMPDIR/x265_${X265_VERSION}
     cmake source/
     make || return 1
     make install || return 1
@@ -214,10 +231,17 @@ function install_bmd {
 
 function install_ffmpeg {
     cd $TEMPDIR
-    wget http://ffmpeg.org/releases/ffmpeg-${FFMPEG_VERSION}.tar.bz2 || return 1
-    echo "Extracting ffmpeg"
-    tar -xf ffmpeg-${FFMPEG_VERSION}.tar.bz2 || return 1
-    cd ffmpeg-${FFMPEG_VERSION}
+    MODULE_NAME="ffmpeg-${FFMPEG_VERSION}"
+    if [ ! -f ${MODULE_NAME}.tar.bz2 ]; then
+        wget http://ffmpeg.org/releases/${MODULE_NAME}.tar.bz2 || return 1
+    fi
+
+    if [ -d ${MODULE_NAME} ]; then
+        rm -rf ${MODULE_NAME}
+    fi
+
+    tar -xf ${MODULE_NAME}.tar.bz2 || return 1
+    cd ${MODULE_NAME}
 
     ./configure --prefix=$PREFIX \
       --enable-nonfree \
@@ -230,7 +254,7 @@ function install_ffmpeg {
     --enable-fontconfig      ` # enable fontconfig, useful for drawtext filter` \
     --enable-frei0r          ` # enable frei0r video filtering` \
     --enable-ladspa          ` # enable LADSPA audio filtering` \
-    --enable-libass          ` # enable libass subtitles rendering, needed for subtitles and ass filter` \
+    --enable-libass          ` # enable libass subtitles rendering` \
     --enable-libbluray       ` # enable BluRay reading using libbluray` \
     --enable-libbs2b         ` # enable bs2b DSP library` \
     --enable-libcaca         ` # enable textual display using libcaca` \
@@ -238,15 +262,13 @@ function install_ffmpeg {
     --enable-libflite        ` # enable flite (voice synthesis) support via libflite` \
     --enable-libfreetype     ` # enable libfreetype, needed for drawtext filter` \
     --enable-libfribidi      ` # enable libfribidi, improves drawtext filter` \
-    --enable-libgme          ` # enable Game Music Emu via libgme` \
     --enable-libmodplug      ` # enable ModPlug via libmodplug` \
     --enable-libmp3lame      ` # enable MP3 encoding via libmp3lame` \
     --enable-libopus         ` # enable Opus de/encoding via libopus` \
-    --enable-libpulse        ` # enable Pulseaudio input via libpulse` \
+    --enable-librtmp         ` # enable LibRTMP` \
     --enable-libsoxr         ` # enable Include libsoxr resampling` \
     --enable-libspeex        ` # enable Speex de/encoding via libspeex` \
     --enable-libssh          ` # enable SFTP protocol via libssh` \
-    --enable-libtheora       ` # enable Theora encoding via libtheora` \
     --enable-libtwolame      ` # enable MP2 encoding via libtwolame` \
     --enable-libv4l2         ` # enable libv4l2/v4l-utils` \
     --enable-libvpx          ` # enable VP8 and VP9 de/encoding via libvpx` \
@@ -257,10 +279,11 @@ function install_ffmpeg {
     --enable-libzvbi         ` # enable teletext support via libzvbi` \
     --enable-decklink        ` # enable Blackmagic DeckLink I/O support` \
     --enable-nvenc           ` # enable NVIDIA NVENC support` \
-    --enable-openssl         ` # enable openssl, needed for https support if gnutls is not used` \
+    --enable-openssl         ` # needed for https support if gnutls is not used` \
+    $CUDA_PARAMS \
     || return 1
 
-
+    echo "Making ffmpeg"
     make || return 1
     make install || return 1
     make clean
